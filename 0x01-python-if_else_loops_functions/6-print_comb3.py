c41fc3b8d928405dc7d014e4f1e3ccc0097c55cd
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-09 18:20:45 +0000

    update print comb function

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(i, 10):
-        if ((i * 10 + j) < 89):
-            print("{}{}".format(i, j), end=", ")
-        else:
-            print("{}{}".format(i, j))
+        if (i != j):
+            if ((i * 10 + j) < 89):
+                print("{}{}".format(i, j), end=", ")
+            else:
+                print("{}{}".format(i, j))
