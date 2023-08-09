fe045d701176b4e4f09fcbae7bbffcfb7d2c2e1d
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-09 19:38:52 +0000

    update the remove char function

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if (len(str) < abs(n)):
+    if (len(str) < n or n < 0):
         return (str)
     else:
         return (str[:n] + str[n + 1:])
