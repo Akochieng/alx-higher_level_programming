a07d348bf6b51b689d008b4353d6d7713c4ee5c2
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-09 12:51:35 +0000

    update pycode style formated version

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,9 +4,9 @@ number = random.randint(-1000, 1000)
 usednum = abs(number)
 str = f"Last digit of {number} is {usednum % 10} and is "
 if ((usednum % 10) == 0):
-	strf = str + f"0"
+    strf = str + f"0"
 elif ((usednum % 10) > 5):
-	strf = str + f"greater than 5"
+    strf = str + f"greater than 5"
 else:
-	strf = str + f"less than 6 and not 0"
+    strf = str + f"less than 6 and not 0"
 print(strf)
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')):
-	if not (i == ord('q') or i == ord('e')):
-		print("{:c}".format(i), end='')
+    if not (i == ord('q') or i == ord('e')):
+        print("{:c}".format(i), end='')
