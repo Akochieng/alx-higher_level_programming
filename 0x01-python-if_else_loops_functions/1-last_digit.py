7f89ceaeee98beea0f9fe067ec0369ccd773d697
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-09 17:45:10 +0000

    update the last digit function

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-1000, 1000)
 usednum = abs(number)
-str = f"Last digit of {number} is {usednum % 10} and is "
+str = f"Last digit of {number:d} is {usednum % 10} and is "
 if ((usednum % 10) == 0):
     strf = str + f"0"
 elif ((usednum % 10) > 5):
