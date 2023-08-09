020c5145b24946b03e15cf70f27f4c61174d2df1
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-09 19:22:36 +0000

    update the tebahpla function

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('`'), -1):
-    if i % 2 == 0:
-        print(chr(i), end="")
-    else:
-        print(chr((i - ord('a')) + ord('A')), end="")
+    i = i - ord('a') + ord('A') if i % 2 != 0 else i
+    print("{:c}".format(i), end="")
