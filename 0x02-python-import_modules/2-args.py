fc1639e28d549e16796bab4108cb6ea9b5578612
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-11 19:13:10 +0000

    make function not run when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
-num = len(argv) - 1
-if num == 0:
-    print(f"{num:d} arguments.")
-elif num == 1:
-    print(f"{num:d} argument:")
-else:
-    print(f"{num:d} arguments:")
-for i in range(1, num + 1):
-    print(f"{i:d}: {argv[i]}")
+if __name__ == "__main__":
+    num = len(argv) - 1
+    if num == 0:
+        print(f"{num:d} arguments.")
+    elif num == 1:
+        print(f"{num:d} argument:")
+    else:
+        print(f"{num:d} arguments:")
+    for i in range(1, num + 1):
+        print(f"{i:d}: {argv[i]}")
