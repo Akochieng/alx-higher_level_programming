7a594b1be8249e361443c74e44bc118ecb2bcca4
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-14 19:50:39 +0000

    update the print matrix function

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for innner_list in matrix:
-        lent = len(inner_list)
-        for i in range(lent)
-            if i < lent - 1
-                print("{:d}".format(i), end=" ")
-            else:
-                print("{:d}".format(i))
+    for inner in matrix:
+        ln = len(inner)
+        if ln == 0:
+            print("")
+        else:
+            for a in range(ln):
+                if a < ln - 1:
+                    print("{:d}".format(inner[a]), end=" ")
+                else:
+                    print("{:d}".format(inner[a]))
