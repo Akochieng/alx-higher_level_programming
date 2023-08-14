cae8055e3d2c4fa6d9819dc0016ab814654ba53d
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-14 17:27:51 +0000

    update function to retrieve variable

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for innner_list in matrix:
+        lent = len(inner_list)
+        for i in range(lent)
+            if i < lent - 1
+                print("{:d}".format(i), end=" ")
+            else:
+                print("{:d}".format(i))
