81752da271b42590dc933e758f1452cf6ac94cde
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-15 19:56:34 +0000

    update no c function

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     templist = list(my_string)
-    for i in templist:
-        if i in "cC":
+    tempcopy = templist[:]
+    for i in tempcopy:
+        if i in "Cc":
             templist.remove(i)
     my_string = "".join(templist)
     return (my_string)
