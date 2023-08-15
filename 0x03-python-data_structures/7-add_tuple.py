f27e921915c8f51a8de77e29022694ff3597866c
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-15 20:19:51 +0000

    update the add tuple function

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    x = extend_tuple(tuple_a)
+    y = extend_tuple(tuple_b)
+    return (x[0] + y[0], x[1] + y[1])
+
+
+def extend_tuple(tuple_c=()):
+    if len(tuple_c) == 1:
+        d = tuple_c[0], 0
+    elif len(tuple_c) == 0:
+        d = 0, 0
+    else:
+        d = tuple_c[:2]
+    return (d)
