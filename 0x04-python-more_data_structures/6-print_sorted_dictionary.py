49c3598b5b69170375f1e88c3c9968aeb5b7c846
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-27 15:47:11 +0000

    update dict printing function

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    newdict = {i: v for i, a_dictionary.get(i) in [map(sorted(a_dictionary))]}
-    print(newdict)
+    sorted_keys = a_dictionary.keys()
+    new_dict = {key: a_dictionary.get(key) for key in sorted_keys}
+    for key, value in new_dict.items():
+        print(f"{key}: {value}")
