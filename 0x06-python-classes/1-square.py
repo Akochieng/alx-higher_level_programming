6b626f1e0404b05c48447f9399bec754499313e2
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-29 18:53:00 +0000

    update private attributes in square instances

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+        '''The class square implements a typical square
+
+        The following parameters are initialised when the class
+        is first initialised.
+
+        Args:
+            size (float): the length or width of the square
+            '''
+        self._size = size
