eda372bf1f1d31fa57288bb8e5b982ca60ee105d
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-30 13:39:34 +0000

    update docstring for the getter and setter methods

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -23,24 +23,24 @@ class Square:
     @property
     def size(self):
         '''size: the length of the square
+        The property has a setter and getter method.
+        The setter method has the following attributes:
+
+        Args:
+            size (int): the length of the square
+
+        Returns:
+            Does not return anythin. It only sets the __size attribute
+
+        Raises:
+            TypeError: If size is not of type int
+            ValueError: If the size is not >= 0
         '''
         return self.__size
     @size.setter
     def size(self, newsize):
-        '''The set_size method defines the __size private attribute.
-        It basically takes in size as a parameter and assigns it to
-        the parameter __size.
-
-        Args:
-            size (int):The size parameter
-
-        Returns:
-            Nothing at all. Just sets the __size attribute
-
-        Raises:
             TypeError: If parameter is not an interger
             ValueError: If parameter is not greater than 0
-        '''
         if not isinstance(newsize, int):
             raise TypeError("size must be an integer")
         elif size < 0:
