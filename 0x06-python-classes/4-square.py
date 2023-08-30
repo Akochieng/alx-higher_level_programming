883e08881236a5998d2523e2ebcdf285daae1a77
Alphonce Kochieng <dulkochieng@gmail.com>
2023-08-30 14:01:03 +0000

    sort indentation error

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,21 +2,22 @@
 '''Square Module
 This module demonstrates how to work with classes.
 
+It includes the square class along with its attributes and methods.
 The functionality included in this module is only for
 demonstration purposes.
 '''
 
 
 class Square:
-        '''The class square implements a typical square
+    '''The class square implements a typical square
 
-        it does not have any class attributes. All instance
-        attributes are defined in the init function below.
-            '''
+    it does not have any class attributes. All instance
+    attributes are defined in the init function below.
+    '''
     def __init__(self, size=0):
         '''
         Args:
-            size (float): the length or width of the square
+            size (int): the length or width of the square
             '''
         self.__size = size
 
@@ -30,17 +31,16 @@ class Square:
             size (int): the length of the square
 
         Returns:
-            Does not return anythin. It only sets the __size attribute
+            self.__size attribute
 
         Raises:
             TypeError: If size is not of type int
             ValueError: If the size is not >= 0
         '''
         return self.__size
+
     @size.setter
     def size(self, newsize):
-            TypeError: If parameter is not an interger
-            ValueError: If parameter is not greater than 0
         if not isinstance(newsize, int):
             raise TypeError("size must be an integer")
         elif size < 0:
