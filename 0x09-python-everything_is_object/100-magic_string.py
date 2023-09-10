c5544bca70f14a6ba3dbd090d9f46bfbfd82371a
Alphonce Kochieng <dulkochieng@gmail.com>
2023-09-10 15:48:37 +0000

    everything in python is an object

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''This module demonstrates how to work with the inspect object'''
+def magic_string():
+    state = 0
+    for a in range (i):
+        if state == 1:
+            print(", ", end="")
+        print("BestSchool", end="")
+        state = 1
+    print("")
