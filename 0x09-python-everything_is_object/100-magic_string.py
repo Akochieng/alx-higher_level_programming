0288c51fbacc869d5c48bc35d0cb571adf7d4529
Alphonce Kochieng <dulkochieng@gmail.com>
2023-09-10 16:35:08 +0000

    working with mutable arguments

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 '''This module demonstrates how to work with the inspect object'''
-def magic_string():
-    state = 0
-    for a in range (i):
-        if state == 1:
-            print(", ", end="")
-        print("BestSchool", end="")
-        state = 1
-    print("")
+def magic_string(_list[]):
+    _list.append("BestSchool")
+    return ", ".join(_list)
