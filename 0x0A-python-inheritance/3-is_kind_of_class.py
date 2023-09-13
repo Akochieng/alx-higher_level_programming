476d116e6318fff8924ea7515aa0ee76ce3bbdd0
Alphonce Kochieng <dulkochieng@gmail.com>
2023-09-13 21:33:38 +0000

    is kind of the class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''helps with checking if the object is an instance of a class'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''checks whether obj is an instance of the a_class
+
+    Args:
+        obj: the object
+        a_class: the class
+
+    Returns: True or False
+    '''
+    return isinstance(obj, a_class)
