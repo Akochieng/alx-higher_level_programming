90f5f6cc733480800854a299255ed5c8c8e0d6ca
Alphonce <dulkochieng@gmail.com>
2023-10-18 05:35:31 +0300

    update show table create script

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- list scores and name for score >= 10 in desc order
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
