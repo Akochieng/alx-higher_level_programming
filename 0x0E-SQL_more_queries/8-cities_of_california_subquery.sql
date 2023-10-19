31b69b2219ba47e22b4b5fb763a0da1219c1dff8
Alphonce <dulkochieng@gmail.com>
2023-10-19 20:23:30 +0300

    use join to select cities and states

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,2 @@
 -- Select all California cities in the table
-USE hbtn_0d_usa;
 SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,2 @@
 -- script that lists all cities contained in a database
-USE hbtn_0d_usa;
 SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id = cities.state_id;
