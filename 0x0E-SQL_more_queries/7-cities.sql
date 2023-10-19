c93eca22a16199d7e4fb79f8dbcee345af4cd494
Alphonce <dulkochieng@gmail.com>
2023-10-19 19:24:36 +0300

    correct the documentation error

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
---creates the database hbtn_0d_usa and the table cities
+-- creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities ( id INT AUTO_INCREMENT,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities ( id INT AUTO_INCREMENT,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
 PRIMARY KEY (ID),
