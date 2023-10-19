98a90ca301269e370ccc19d9eabbbd209945328a
Alphonce <dulkochieng@gmail.com>
2023-10-19 23:58:35 +0300

    update tv shows script

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- list all comedy shows
+SELECT title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy";
