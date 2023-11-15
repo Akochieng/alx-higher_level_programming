daafe30677ea7224aad0e7ffa3f494e9c989a383
Alphonce <dulkochieng@gmail.com>
2023-11-15 18:51:03 +0300

    update 6-square to use extends

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,19 @@
 #!/usr/bin/node
 
-const Square = require('./5-square');
-Square.prototype.charPrint = function (c) {
-  let str = '';
-  if (c === undefined) { c = 'X'; }
-  for (let h = 0; h < this.height; h++) {
-    for (let w = 0; w < this.width; w++) { str += c; }
-    if (h < this.height - 1) { str += '\n'; }
+const Sq = require('./5-square');
+class Square extends Sq {
+  constructor (width) {
+    super(width, width);
   }
-  console.log(str);
-};
 
+  charPrint = function (c) {
+    let str = '';
+    if (c === undefined) { c = 'X'; }
+    for (let h = 0; h < this.height; h++) {
+      for (let w = 0; w < this.width; w++) { str += c; }
+      if (h < this.height - 1) { str += '\n'; }
+    }
+    console.log(str);
+  };
+}
 module.exports = Square;
