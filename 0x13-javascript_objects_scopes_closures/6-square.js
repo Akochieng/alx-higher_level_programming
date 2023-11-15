f8138e477ea602bf3392118da5bce5abb3ff40b1
Alphonce <dulkochieng@gmail.com>
2023-11-15 18:44:22 +0300

    update square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@
 const Square = require('./5-square');
 Square.prototype.charPrint = function (c) {
   let str = '';
-  if (c === undefined) { c = 'x'; }
+  if (c === undefined) { c = 'X'; }
   for (let h = 0; h < this.height; h++) {
     for (let w = 0; w < this.width; w++) { str += c; }
     if (h < this.height - 1) { str += '\n'; }
